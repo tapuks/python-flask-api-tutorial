5665ad8e838c2518eca308b95876e27b6b1ece93
tapuks <david_berdiell@hotmail.com>
2021-06-03 18:36:49 +0000

    firts app route

--- src/app.py
+++ src/app.py
@@ -1,2 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
+
+# These two lines should always be at the end of your app.py file.
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return "<h1>Hello!</h1>"
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
